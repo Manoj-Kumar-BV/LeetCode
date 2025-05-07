747e2381fcd000d6085c4233d2a48bb02b54d2fe
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2025-05-07 11:21:38 +0530

    Time: 0 ms (100%), Space: 43.4 MB (16.06%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double base = x;
+        int exp = n;
+        double result = Math.pow(base,exp);
+        return result;
+    }
+}

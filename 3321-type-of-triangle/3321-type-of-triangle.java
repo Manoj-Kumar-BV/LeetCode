2ede0a4105f332f8a6b162d80818920218acb6b1
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2025-05-23 20:32:30 +0530

    Time: 1 ms (54.83%), Space: 42.4 MB (15.78%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.java
+++ 3321-type-of-triangle/3321-type-of-triangle.java
@@ -1,39 +1,13 @@
 class Solution {
     public String triangleType(int[] nums) {
-        if (!((nums[0] + nums[1] > nums[2]) && (nums[0] + nums[2] > nums[1]) && (nums[1] + nums[2] > nums[0]))){
-            return "none";
-            
-        }else{
-            if(nums[0] == nums[1] && nums[1] == nums[2]){
-                return "equilateral";
-            }else if((nums[0] == nums[1]) || (nums[0] == nums[2] || nums[1] == nums[2])){
-                return "isosceles";
-            }else{
-                return "scalene";
-            }
-        }
-    }
-}
-
-/*
-class Solution {
-    public String triangleType(int[] nums) {
-
-        int a = nums[0];
-        int b = nums[1];
-        int c = nums[2];
-
-        if(a==b && b==c) {
+        if(nums[0] == nums[1] && nums[1] == nums[2]){
             return "equilateral";
-        } else if (a+b<=c || b+c<=a || c+a<=b) {
+        }else if(nums[0]+nums[1] <= nums[2] || nums[1]+nums[2] <= nums[0] | nums[0]+nums[2] <= nums[1]){
             return "none";
-        } else if(a==b || b==c || c==a){
+        }else if(nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2]){
             return "isosceles";
-        } else {
+        }else{
             return "scalene";
         }
     }
 }
-
-Better Approach
-*/

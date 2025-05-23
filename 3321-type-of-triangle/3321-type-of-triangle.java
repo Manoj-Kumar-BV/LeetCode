370a1e289a8633d09585f00b4b4bcdac48afc18c
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2025-05-23 20:23:47 +0530

    Update 3321-type-of-triangle.java

--- 3321-type-of-triangle/3321-type-of-triangle.java
+++ 3321-type-of-triangle/3321-type-of-triangle.java
@@ -14,3 +14,26 @@ class Solution {
         }
     }
 }
+
+/*
+class Solution {
+    public String triangleType(int[] nums) {
+
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+
+        if(a==b && b==c) {
+            return "equilateral";
+        } else if (a+b<=c || b+c<=a || c+a<=b) {
+            return "none";
+        } else if(a==b || b==c || c==a){
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+}
+
+Better Approach
+*/

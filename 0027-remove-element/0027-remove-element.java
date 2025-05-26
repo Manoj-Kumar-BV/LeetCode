70847b20e4ba9e94fcac8b2e0ba882d45a4aa14e
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2025-05-26 19:36:08 +0530

    Time: 0 ms (100%), Space: 42.2 MB (30.7%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int n1 = nums.length;
-        int[] nums2 = new int[n1];
+        // int[] nums2 = new int[n1];
 
         int k = 0;
         for(int i = 0; i<n1; i++){

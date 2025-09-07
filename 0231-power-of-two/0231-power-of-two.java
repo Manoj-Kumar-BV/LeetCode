291ff93d6f0bcbbc09ed9dcad011abfc00e44357
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2025-09-07 05:55:37 +0530

    Time: 0 ms (100%), Space: 40.8 MB (67.32%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        // return (n > 0) && (n & (n-1)) == 0;
-        if(n > 0){
-            if ((n &(n-1)) == 0){
-                return true;
-            }
+        if(n>0){
+            if((n & (n-1)) == 0){
+            return true;
         }
-        
+        }
         return false;
     }
 }

a65a0355a8ece457f0b3ccade12feb80ef2b27c0
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2025-09-10 19:32:16 +0530

    Time: 2 ms (87.2%), Space: 56.1 MB (26.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,40 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // int[] result = new int[nums.length];
-        // for(int i=0; i<nums.length; i++){
-        //     int product = 1;
-        //     for(int j=0; j<nums.length; j++){
-        //         if(j != i){
-        //             product *= nums[j];
-        //         }
-        //     }
-        //     result[i] = product;
-        // }
-        // return result;
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+
+        prefix[0] = nums[0];
+        for(int i=1; i<nums.length; i++){
+            prefix[i] = prefix[i-1] * nums[i];
+        }
+
+        suffix[nums.length-1] = nums[nums.length-1];
+        for(int i=nums.length-2; i>=0; i--){
+            suffix[i] = suffix[i+1] * nums[i];
+        }
 
         int[] result = new int[nums.length];
-        int prefixprod[] = new int[nums.length];
-        int suffixprod[] = new int[nums.length];
-
-        prefixprod[0] = nums[0];
-        suffixprod[nums.length-1] = nums[nums.length-1];
-
-        for(int i=1; i<nums.length; i++){
-            prefixprod[i] = prefixprod[i-1] * nums[i]; 
-        }
-
-        for(int i=nums.length-2; i>=0; i--){
-            suffixprod[i] = nums[i] * suffixprod[i+1];
-        }
-
-        result[0] = suffixprod[1];
-
-        result[nums.length-1] = prefixprod[nums.length-2];
-
+        result[0] = suffix[1];
+        result[nums.length-1] = prefix[nums.length-2];
         for(int i=1; i<nums.length-1; i++){
-            result[i] = prefixprod[i-1] * suffixprod[i+1];
+            result[i] = prefix[i-1] * suffix[i+1];
         }
-
         return result;
     }
 }

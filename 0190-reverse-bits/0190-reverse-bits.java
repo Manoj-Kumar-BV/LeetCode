05c911ca5ddf044995c70d2a2fddbcd7682fbbee
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2025-09-26 10:45:47 +0530

    Time: 0 ms (100%), Space: 40.5 MB (96.08%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,13 +1,10 @@
-public class Solution {
-    
+class Solution {
     public int reverseBits(int n) {
         int res = 0;
-        for (int i = 0; i < 32; i++) {  
-            int ld = n & 1;  
+        for(int i=0; i<32; i++){
+            int ld = n & 1;
             res = (res << 1) | ld;
-            
-            
-            n >>= 1;
+            n = n>>1;
         }
         return res;
     }
